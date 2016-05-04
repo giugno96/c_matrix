cf347ba66eb51e5610a0d9ebcf1603addd61dc4d
peppuxxo96 <giuseppe-gnf@hotmail.it>
2016-05-05 01:48:53 +0200

    Create matrix.h

---
+++ matrix.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+//This function returns a pointer to a matrix[rows][cols]
+char **matAlloc(int rows, int cols, int dim)
+{
+	char **m = (char **)calloc(cols, sizeof(char *));
+	int i;
+	for(i = 0; i < rows; i++)
+		m[i] = (char *)calloc(rows, dim);
+	return m;
+}
